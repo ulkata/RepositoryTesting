005ced6da8f0df909f2f4509a52496e19f125f34
ulkata <j.mihalkov2019@pmg-pernik.com>
2023-02-16 10:26:06 +0200

    add coment

---
+++ BankinkSystem.Tests/BankAccountTests.cs
@@ -0,0 +1,47 @@
+﻿using Bankinksystem;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankinkSystem.Tests
+{
+    [TestFixture]
+    class BankAccountTests
+    {
+        [Test]
+        public void DepositShouldIncreaseBalance()
+        {
+            {
+                BankAccount bankAccount = new BankAccount(123);
+                decimal depositAmount = 100;
+
+
+                bankAccount.Deposit(depositAmount);
+
+                Assert.AreEqual(depositAmount, bankAccount.Balance);
+
+
+
+
+            }
+        }
+            [Test]
+            public void ConstructorShouldSetZeroBalance()
+            {
+                int id = 123;
+                BankAccount account = new BankAccount(id);
+                Assert.AreEqual(0, account.Balance);
+            }
+
+        
+        [Test]
+        public void AccountInicializeWithPositiveValue()
+        {
+            BankAccount bankAccount = new BankAccount(123,2000m);
+            Assert.AreEqual(2000m, bankAccount.Balance);
+        }
+    }
+}
